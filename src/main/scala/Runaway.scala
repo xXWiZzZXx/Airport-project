c114ad8d6763391a9eb13b65f0a41d406397fe49
xXWiZzZXx <samuel.sidoun@edu.ece.fr>
2025-04-06 21:25:59 +0200

    Changes of runaway, it had a bug that caused it to not work with encoding.

--- src/main/scala/Runaway.scala
+++ src/main/scala/Runaway.scala
@@ -5,9 +5,17 @@ final case class Runway(id: String, airportRef: String, surface: String, leIdent
 object Runway {
   def fromCsv(line: String): Option[Runway] = {
     line.split(",", -1).toList match {
-      case id :: airportRef :: airportIdent :: lenFt :: widthFt :: surface :: lighted :: closed :: leIdent :: _ =>
-        Some(Runway(id.trim, airportRef.trim, surface.trim, leIdent.trim))
+      case id :: airportRef :: _ :: _ :: _ :: surface :: _ :: _ :: leIdent :: _ =>
+        Some(
+          Runway(
+            id.trim,
+            airportRef.trim,
+            surface.replace("\"", "").trim,
+            leIdent.replace("\"", "").trim
+          )
+        )
       case _ => None
     }
   }
 }
+
